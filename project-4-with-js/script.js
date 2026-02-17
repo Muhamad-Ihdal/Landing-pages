cbaf9d3ccc413a33f4de1c044fd5ea9038585a8f
muhammad ihdal <muhammadihdal300@gmail.com>
2026-02-17 17:45:12 +0700

    project-4 jadi

--- project-4-with-js/script.js
+++ project-4-with-js/script.js
@@ -8,4 +8,22 @@ btnAdd.addEventListener("click", function(){
         return
     }
     
+    const card = document.createElement("div")
+
+    const note = document.createElement("p")
+    note.textContent = inputNote.value
+    
+    const btnRmv = document.createElement("button")
+    btnRmv.textContent = "Hapus"
+    
+    btnRmv.addEventListener("click",function(){
+        card.remove()
+    })
+    
+    card.appendChild(note)
+    card.appendChild(btnRmv)
+    container.appendChild(card)
+
+    inputNote.value = ""
+    
 })
