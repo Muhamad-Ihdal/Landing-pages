f669b63c556a282558d142f2eb516cd89cb22ffa
muhammad ihdal <muhammadihdal300@gmail.com>
2026-03-01 16:10:27 +0700

    project 6 gw udah update gila gilaan

--- project-6-remake-todo/script.js
+++ project-6-remake-todo/script.js
@@ -2,8 +2,13 @@ let todoInput = document.querySelector("#todoInput")
 let btnAdd = document.querySelector("#btnAdd")
 let todoContainer = document.querySelector("#todoContainer")
 let btnRmvAll = document.querySelector("#btnRemoveAll")
+let todos = []
+let unique_id = 1; 
 
-btnRmvAll.addEventListener("click",() => todoContainer.innerHTML = "")
+btnRmvAll.addEventListener("click",() => {
+    todoContainer.innerHTML = "";
+    todos = [];
+})
 
 btnAdd.addEventListener('click',function(event){
     event.preventDefault();
@@ -11,17 +16,8 @@ btnAdd.addEventListener('click',function(event){
         return
     }
 
-    let todoDiv = createTodoDiv()
-    let checkbox = createCheckbox()
-    let textTodo = createText()
-    let btnRmv = createBtnRmv()
-
-    todoDiv.appendChild(checkbox)
-    todoDiv.appendChild(textTodo)
-    todoDiv.appendChild(btnRmv)
-    todoContainer.appendChild(todoDiv)
-
-
+    addTodo(text=todoInput.value.trim())
+    console.log(todos)
     todoInput.value = ''
 })
 
@@ -29,34 +25,103 @@ btnAdd.addEventListener('click',function(event){
 todoContainer.addEventListener("click",(e)=>{
     if (e.target.matches(".btn-rmv")){
         let todo = e.target.closest(".todo")
-        todo.remove()
+        let todoId = Number(todo.dataset.id)
+        removeFromArr(todoId)
+
         return
     }
-    let todo = e.target.closest(".todo")
-    coretTodo(todo)
+    
+    if (!e.target.matches("input")){
+        let todo = e.target.closest(".todo")
+        coretTodo(todo)
+    }
 
 
 })
 
 todoContainer.addEventListener("change",(e)=>{
+    if (e.target.type !== "checkbox") return
+    
     let todo = e.target.closest(".todo")
     coretTodo(todo)
 })
 
 
+function addTodo(text){
+    let data = {
+        id: unique_id,
+        text:text,
+        completed:false
+    };
+    unique_id++;
+    todos.push(data);
 
-function coretTodo(todo){
-    let text = todo.querySelector('p')
-    let checkbox = todo.querySelector("input")
-    
-    if (checkbox.checked){
-        checkbox.checked = false
-    }
-    else{
-        checkbox.checked = true
-    }
-    return text.classList.toggle("done",checkbox.checked)
+    renderTodo()
 }
+
+function renderTodo(){
+    todoContainer.innerHTML = "";
+
+    todos.forEach((todo) => {
+        let todoDiv = createTodoDiv()
+        let checkbox = createCheckbox()
+        let textTodo = createText(todo.text)
+        let btnRmv = createBtnRmv()
+
+        todoDiv.dataset.id = todo.id
+
+        todoDiv.appendChild(checkbox)
+        todoDiv.appendChild(textTodo)
+        todoDiv.appendChild(btnRmv)
+        todoContainer.appendChild(todoDiv)
+
+        if (todo.completed === true){
+            textTodo.classList.add("done")
+            checkbox.checked = true
+        }
+    })
+
+}
+
+function removeFromArr(id){
+    for (let i in todos){
+        if (todos[i].id === id){
+            delete todos[i]
+        }
+    }
+}
+
+
+console.log
+function coretTodo(todo){
+    let checkbox = todo.querySelector("input")
+    let id = Number(todo.dataset.id)
+    
+    
+    
+    for (let i in todos){
+
+        if (todos[i].id === id && todos[i].completed === false ){
+            todos[i].completed = true;
+            console.log("consol di ubah jadi true")
+        } else if (todos[i].id === id && todos[i].completed === true ){
+            todos[i].completed = false;
+            console.log("consol di ubah jadi false")
+        }
+
+
+        if (todos[i].completed === true){
+            checkbox.checked = true
+        } else{
+            checkbox.checked = false
+        }
+    }
+
+    renderTodo()
+}
+
+
+
 
 function createTodoDiv(){
     let todo = document.createElement("div")
@@ -70,9 +135,9 @@ function createCheckbox(){
     return checkbox
 }
 
-function createText(){
+function createText(text){
     let textTodo = document.createElement("p")
-    textTodo.textContent = todoInput.value.trim()
+    textTodo.textContent = text.trim()
     return textTodo 
 }
 
@@ -82,6 +147,7 @@ function createBtnRmv(){
     btnRmv.classList.add("btn-rmv")
     return btnRmv
 }
+
 
 
 
