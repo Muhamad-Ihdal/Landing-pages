9d8ed13a103763ec14ea8086df005ff18d9d5358
muhammad ihdal <muhammadihdal300@gmail.com>
2026-03-07 20:55:52 +0700

    project 7  tampilan udah jadi, dan fitur add product juga udah jadi

---
+++ project-7/script.js
@@ -0,0 +1,78 @@
+let productNameInput = document.querySelector("#productNameInput")
+let stockInput = document.querySelector("#stockInput")
+let btnAddProduct = document.querySelector("#btnAddProduct")
+let products;
+loadData()
+console.log(products)
+
+
+
+// ----------------------------- local storage ----------------------
+function saveData(){
+    localStorage.setItem("products",JSON.stringify(products))
+}
+
+function loadData(){
+    let data = localStorage.getItem("products")
+    if (data) {
+        products = JSON.parse(data)
+    } else {
+        products = []
+    }
+}
+// ----------------------------- local storage end----------------------
+
+
+
+// ----------------------------- Add Product ----------------------
+btnAddProduct.addEventListener("click",(e)=>{
+    e.preventDefault();
+    if (!productNameInput.value.trim()) return
+    else if (!stockInput.value.trim() || isNaN(stockInput.value) || Number(stockInput.value) < 0) {
+        alert("input must be a positive number")
+        return
+    }
+    
+    let id;
+    do{
+        id = generateId()
+    } while (isIdExits(id))
+        
+        
+    products.push({
+        id:id,
+        name:productNameInput.value.trim(),
+        stock:Number(stockInput.value)
+    })
+    saveData()
+    
+    productNameInput.value = ""
+    stockInput.value = ""
+    console.log(products)
+    })
+    
+
+function generateId(){
+    return Math.random().toString(36).substring(2, 8);
+}
+
+
+
+function isIdExits(newId){
+    if (products.length === 0) {return false;}
+    
+    let duplicatId = products.find(d => d.id === newId)
+    
+    if (duplicatId) {alert("dupclicate ditemukan!!!");return true}
+    else {return false}
+}
+
+
+// ----------------------------- Add Product end----------------------
+
+
+
+// ----------------------------- Render Data ----------------------
+
+// ----------------------------- Render Data end----------------------
+
