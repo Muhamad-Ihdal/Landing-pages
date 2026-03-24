460fe9ea678bfc78e53f53db1e2bad9954ece1b8
muhammad ihdal <muhammadihdal300@gmail.com>
2026-03-24 12:50:08 +0700

    project 9 udah setengah jadi

--- project-9/script.js
+++ project-9/script.js
@@ -2,14 +2,20 @@ let categories;
 let transactions;
 let selectCategory = document.querySelector("#selectCategory");
 let selectFilterCategory = document.querySelector("#selectFilterCategory");
+let tbodyCategories = document.querySelector("#tbodyCategories");
 
 loadData();
-renderSelect();
+renderCategoriesAndSelect();
+feather.replace();
 
-// ------------- localStorage
+// -------------------------------- localStorage
 function saveData() {
   localStorage.setItem("categories", JSON.stringify(categories));
   localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+function filterDefault() {
+  transactions.filter = "";
 }
 
 function loadData() {
@@ -27,23 +33,29 @@ function loadData() {
   } else {
     transactions = {
       main: [],
-      filter: "",
+      filter: "all",
     };
   }
 }
-// ------------- localStorage End
+// -------------------------------- localStorage End
 
-// ------------- utils
+// -------------------------------- utils
+
+function renderCategoriesAndSelect() {
+  renderCategories();
+  renderSelect();
+}
+
 function normalize(text) {
   return text.toLowerCase().replace(" ", "");
 }
 
 function generateId() {
-  return Math.random().toString(36).substring(2, 10);
+  return Math.random().toString(36).substring(2, 8);
 }
-// ------------- utils end
+// -------------------------------- utils end
 
-// ------------- form input
+// -------------------------------- form input
 // input category
 
 let categoryInput = document.querySelector("#inputKategori");
@@ -53,7 +65,7 @@ saveCategoryBtn.addEventListener("click", addCategory);
 
 function addCategory(e) {
   e.preventDefault();
-  let categoryName = categoryInput.value;
+  let categoryName = categoryInput.value.trim();
   if (!categoryInput.value.trim()) {
     categoryInput.value = "";
     return;
@@ -74,6 +86,7 @@ function addCategory(e) {
   console.log("Successfully added");
   console.log(categories);
   renderSelect();
+  renderCategories();
   saveData();
   categoryInput.value = "";
 }
@@ -90,8 +103,15 @@ function addTransaction(e) {
   let transactionValue = transactionValueInput.value;
   let note = noteInput.value;
   let currentCategory = selectCategory.value;
+  const ctgr = categories.find((c) => c.name === currentCategory);
+  console.log(ctgr);
+  console.log(ctgr.id);
   const transactionId = generateId();
-  
+  if (!currentCategory) {
+    alert("category is not exist");
+    return;
+  }
+
   if (!note.trim() || !transactionValue.trim()) {
     noteInput.value = "";
     return;
@@ -99,30 +119,75 @@ function addTransaction(e) {
 
   transactions.main.push({
     trId: transactionId,
+    categoryId: ctgr.id,
     note: note,
     category: currentCategory,
-    value: transactionValue
+    value: transactionValue,
   });
 
   console.log("Transaction Successfully added");
   console.log(transactions);
-  saveData()
+  console.log(categories);
+  saveData();
   transactionValueInput.value = "";
   noteInput.value = "";
 }
 
-// ------------- form input End
+// -------------------------------- form input End
 
-// ------------- render
+// -------------------------------- render
 
+// render select
 function renderSelect() {
   selectCategory.innerHTML = categories.map(selectTemplate).join("");
   selectFilterCategory.innerHTML =
-    `<option value="all" >All</option>` + categories.map(selectTemplate).join("");
+    `<option value="all" >All</option>` +
+    categories.map(selectTemplate).join("");
+  filterDefault();
 }
 
 function selectTemplate(item) {
-  return `<option value="${item.name}" >${item.name}</option>`;
+  return `<option data-id="${item.id}" value="${item.name}" >${item.name}</option>`; //------------------------------------------
 }
 
-// ------------- render End
+// render category
+function renderCategories() {
+  if (categories.length === 0) {
+    tbodyCategories.innerHTML = `<tr><td colspan="4" class="empty">Category is empty</td></tr>`;
+  } else {
+    tbodyCategories.innerHTML = categories.map(categoriesTemplate).join("");
+    feather.replace();
+  }
+}
+
+tbodyCategories.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.closest("#rmvBtn")) {
+    let id = e.target.closest("tr").dataset.id;
+    removeCategory(id);
+  }
+});
+
+function categoriesTemplate(data) {
+  return `
+  <tr data-id="${data.id}" >
+    <td>${data.id}</td>
+    <td>${data.name}</td>
+    <td>Belum dibikin</td>
+    <td>
+      <a href="#" id="rmvBtn"><i data-feather="trash" class="rmv-btn" >
+      </i></a>
+    </td>
+  </tr>`;
+}
+
+// -------------------------------- render End
+
+// -------------------------------- remove
+function removeCategory(id) {
+  categories = categories.filter((c) => c.id !== id);
+  console.log(categories);
+  saveData();
+  renderCategoriesAndSelect();
+}
+// -------------------------------- remove end
