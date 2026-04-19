eb1c302aef21f43991238bb52eec909d11c790cc
muhammad ihdal <muhammadihdal300@gmail.com>
2026-04-19 15:54:38 +0700

    project 11 html jadi!

--- project_10-19/project-11-react-2/src/App.jsx
+++ project_10-19/project-11-react-2/src/App.jsx
@@ -6,24 +6,25 @@ import { useState } from "react";
 export default function App() {
   const [transaction, setTransaction] = useState([
     {
-      id:'dfdsa',
-      type:'income',
-      amount:200000,
-      desc:'gajian',
-      isDeleted:false,
+      id: "TRX-dfdsa",
+      type: "income",
+      amount: 200000,
+      desc: "gajian",
+      isDeleted: false,
     },
     {
-      id:'dfd432',
-      type:'expense',
-      amount:10000,
-      desc:'beli bensin',
-      isDeleted:false,
-    }
+      id: "TRX-dfd432",
+      type: "expense",
+      amount: 10000,
+      desc: "beli bensin",
+      isDeleted: false,
+    },
   ]);
 
-  // function handleForm() {
+  function handleForm() {
+    setTransaction([])
 
-  // }
+  }
   return (
     <div className="container">
       <section className="balanceCardContainer">
@@ -42,13 +43,21 @@ export default function App() {
         </div>
       </section>
 
-
       <form className="trFormContainer">
         <label htmlFor="descInput">Description:</label>
-        <input type="text" placeholder="Masukan penjelasan..." id="descInput" required/>
+        <input
+          type="text"
+          placeholder="Masukan penjelasan..."
+          id="descInput"
+          required
+        />
 
         <label htmlFor="amountInput">Amount(Rp):</label>
-        <input type="number" id="amountInput" placeholder="Input Rupiah(IDR)..." />
+        <input
+          type="number"
+          id="amountInput"
+          placeholder="Input Rupiah(IDR)..."
+        />
 
         <label htmlFor="typeDropdown">Type:</label>
         <select id="typeDropdown">
@@ -58,7 +67,32 @@ export default function App() {
       </form>
 
       <section className="trListContainer">
-        {transaction.map()}
+        <h1>History transaction</h1>
+        {transaction.map((tr) => {
+          return (
+            <>
+              <article className="transactionCard">
+                <div className={`statusIndicator ${tr.type === 'expense' ? 'expense': 'income'}`}></div>
+
+                <div className="cardContent">
+                  <div className="infoMain">
+                    <span className="idTag">#{tr.id}</span>
+                    <h4 className="description">{tr.desc}</h4>
+                    <span className="typeLabel">{tr.type}</span>
+                  </div>
+
+                  <div className="infoAmount">
+                    <p className="amount">{ tr.type === 'expense' ? "-":"+"} Rp {tr.amount}</p>
+                  </div>
+                </div>
+
+                <button className="deleteBtn" aria-label="Hapus transaksi" onClick={handleForm}>
+                  &times;
+                </button>
+              </article>
+            </>
+          );
+        })}
       </section>
     </div>
   );
